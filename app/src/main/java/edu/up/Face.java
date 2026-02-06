580a0521d92a414dab488eacda91b78762ab05d9
Farid Socias <faridsocias1@gmail.com>
2026-02-05 20:58:46 -0800

    1. Created SurfaceView 2. Added the seekBars for the colors 3. Added the spinner for Hair Style 4. Added the RadioGroup 5. Added botton to create random Face 6. Create Java class: Face & created private ints +++++++ 7. Added constructor and 2 methods for Face.java

--- app/src/main/java/edu/up/Face.java
+++ app/src/main/java/edu/up/Face.java
@@ -1,8 +1,38 @@
 package edu.up;
+import android.graphics.Color;
+import java.util.Random;
+
+/**
+ * DATE: 2/5/2026
+ * @author Farid Socias
+ */
 
 public class Face {
     private int skinColor;
     private int eyeColor;
     private int hairColor;
-    private int hairStyle
+    private int hairStyle;
+    private final Random rng = new Random();
+
+    // Simple constructor for Face
+    public Face(){
+        randomize();
+    }
+
+    //Method for constructor use a random color for the skin, eyes, hair. Also a random for hairStyle
+    public void randomize() {
+        skinColor = randomColorInt();
+        eyeColor = randomColorInt();
+        hairColor = randomColorInt();
+        // Bound: 5 because it could be 5 different styles
+        hairStyle = rng.nextInt(5);
+    }
+
+    //Method needed for randomize() to use colors
+    private int randomColorInt() {
+        int r = rng.nextInt(256);
+        int g = rng.nextInt(256);
+        int b = rng.nextInt(256);
+        return Color.rgb(r, g, b);
+    }
 }
