dbf18a809282485f07f60e2b0fd4a4965ec9a4ab
Farid Socias <faridsocias1@gmail.com>
2026-02-05 21:23:42 -0800

    1. Created SurfaceView 2. Added the seekBars for the colors 3. Added the spinner for Hair Style 4. Added the RadioGroup 5. Added botton to create random Face 6. Create Java class: Face & created private ints 7. Added constructor and 2 methods for Face.java 8. Few things:    -Fixed activity_main.xml    -Added onDraw    -Added Helpers for onDraw    -Added @author to the java files 9. Added strings for Spinner in strings.xml ++++++ 10. Added event actions for Spinner in MainActivity.java

--- app/src/main/java/edu/up/MainActivity.java
+++ app/src/main/java/edu/up/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.up;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //This will give Spinner the events for options
+        Spinner hairSpinner = findViewById(R.id.hairSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hair_styles, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hairSpinner.setAdapter(adapter);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
